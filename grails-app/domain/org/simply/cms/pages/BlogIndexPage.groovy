ca08f152004793a557ededfd1170b672ea27ef1b
sudhir nimavat <sudhir@nimavat.me>
2017-07-16 21:53:25 +0530

    Add previous next page links to blog index

--- grails-app/domain/org/simply/cms/pages/BlogIndexPage.groovy
+++ grails-app/domain/org/simply/cms/pages/BlogIndexPage.groovy
@@ -2,6 +2,7 @@ package org.simply.cms.pages
 
 import grails.compiler.GrailsCompileStatic
 import grails.gorm.PagedResultList
+import grails.plugins.crudify.core.Pager
 import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
 import groovy.transform.TypeCheckingMode
@@ -25,17 +26,17 @@ class BlogIndexPage extends Page implements RoutablePageTrait {
 	@CompileDynamic
 	RouteResult index(Map params) {
 		Map context = super.getContext(null, params)
-		int max = params.int("max") ?: 10
-		int offset = params.int("offset") ?: 0
 
-		PagedResultList<BlogPage> pages = getChilds(-1).list(max: max, offset: offset) {
+		Pager pager = new Pager(params)
+		PagedResultList<BlogPage> pages = getChilds(-1).list(max: pager.max, offset: pager.offset) {
 			eq "published", true
 			order("dateCreated", "desc")
 		}
 
 		context['childs'] = pages
 		context['totalCount'] = pages.totalCount
-
+		context['currentPage'] = params.int('page') ?: 1
+		context['totalPageCount'] = pager.getTotalPages(pages.totalCount)
 		return new RouteResult(this, templateName, context)
 	}
 
--- grails-app/views/pages/blogIndexPage/display.gsp
+++ grails-app/views/pages/blogIndexPage/display.gsp
@@ -14,6 +14,18 @@
 		<p class="blog-intro">${p.intro}</p>
 	</div>
 </g:each>
+
+<nav>
+	<ul class="pager">
+		<g:if test="${currentPage > 1}">
+			<li class="previous"><a href="${request.forwardURI}?page=${currentPage - 1}"><span aria-hidden="true">&larr;</span> Previous</a></li>
+		</g:if>
+		<g:if test="${currentPage < totalPageCount}">
+			<li class="next"><a href="${request.forwardURI}?page=${currentPage + 1}">Next <span aria-hidden="true">&rarr;</span></a></li>
+		</g:if>
+	</ul>
+</nav>
+
 </body>
 </html>
 
